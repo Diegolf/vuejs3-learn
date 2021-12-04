e3d341aa8cb8d520f3a8242dca47dabfc5e65f95
Diego Leonardo <diegolf125@gmail.com>
2021-12-04 15:36:49 -0300

    Directives and loops

--- Project 1 - Self Service Machine/scripts.js
+++ Project 1 - Self Service Machine/scripts.js
@@ -61,6 +61,22 @@ var products = [
         "price": 1.49,
         "active": false,
         "quantity": 1
+    },
+    {
+        "photo": "img/water.png",
+        "name": "Água com gás",
+        "price": 2,
+        "active": false,
+        "quantity": 1
     }
 ];
- 
+
+const SelfServiceMachine = {
+    data() {
+        return {
+            products: window.products
+        }
+    }
+};
+
+Vue.createApp(SelfServiceMachine).mount('#app');
