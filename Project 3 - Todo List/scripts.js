b9ef25b7ea19848cd2426d398d04d9889c30b59b
Diego Leonardo <diegolf125@gmail.com>
2021-12-08 15:01:01 -0300

    Vue Lifecycicle; Store Todo List in Localstorage

--- Project 3 - Todo List/scripts.js
+++ Project 3 - Todo List/scripts.js
@@ -5,17 +5,42 @@ const todoApp = {
             newTodoItem: {}
         }
     },
+    created() {
+        // Carrega a lista do localstorage antes de "montar" o componente
+        const storedTodoList = localStorage.getItem("todoList");
+        this.todoList = storedTodoList ? JSON.parse(storedTodoList) : this.todoList;
+    },
+    beforeUnmount() {
+        // Armazena a lista no localstorage antes de fechar o componente
+        localStorage.setItem("todoList", JSON.stringify(this.todoList));
+    },
+    beforeUpdate(){
+        console.log('beforeUpdate');
+    },
     methods: {
-        orderedTodoList: function() {
-            return this.todoList.sort((todo1, todo2) => (todo1.done === todo2.done) ? 0 : todo1.done ? 1 : -1);
+        orderTodoList: function () {
+            this.todoList.sort((todo1, todo2) => (todo1.done === todo2.done) ? 0 : todo1.done ? 1 : -1);
         },
-        addTodoItem: function() {
-            if (!this.newTodoItem.description || this.newTodoItem.description.trim() === ""){
+        getOrderedTodoList: function () {
+            return [...this.todoList].sort((todo1, todo2) => (todo1.done === todo2.done) ? 0 : todo1.done ? 1 : -1);
+        },
+        addTodoItem: function () {
+            if (!this.newTodoItem.description || this.newTodoItem.description.trim() === "") {
                 alert("Informe a descrição da tarefa.");
                 return;
             }
             this.todoList.push(this.newTodoItem);
+            this.orderTodoList();
+            this.storeTodoList();
             this.newTodoItem = {};
+        },
+        toggleTodoItemDone: function (todoItem) {
+            todoItem.done = !todoItem.done;
+            this.orderTodoList();
+            this.storeTodoList();
+        },
+        storeTodoList: function() {
+            localStorage.setItem("todoList", JSON.stringify(this.todoList));
         }
     }
 }
